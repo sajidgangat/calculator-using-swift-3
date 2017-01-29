c1b6b281364473668ba7735154d57e40aa013337
Sajid gangat <sajidgangat@yahoo.co.in>
2017-01-28 19:17:14 -0800

    Initial Commit

---
+++ calc_300921136/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calc_300921136
+//
+//  Created by Sajid gangat on 2017-01-28.
+//  Copyright © 2017 Sajid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
